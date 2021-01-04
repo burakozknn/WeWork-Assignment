c5458746ce45cb0bae5226379491b71735e6646a
Ozkan, Burak (Synchrony) <Burak.Ozkan@syf.com>
2021-01-04 01:57:47 -0500

    Uploading the assignment

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksAdapter.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import android.content.Context
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.databinding.TaskItemBinding
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksAdapter.ViewHolder
+
+/**
+ * Adapter for the task list. Has a reference to the [TasksViewModel] to send actions back to it.
+ */
+class TasksAdapter(private val viewModel: TasksViewModel,val context: Context) :
+    ListAdapter<Task, ViewHolder>(TaskDiffCallback()) {
+
+    lateinit var timer: CountDownTimer
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+
+        holder.bind(viewModel, item, context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(val binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private lateinit var timer: CountDownTimer
+
+        fun bind(viewModel: TasksViewModel, item: Task, context: Context) {
+
+            binding.apply {
+                viewmodel = viewModel
+                task = item
+                executePendingBindings()
+
+                //setting the countdown timer
+
+                timer = object : CountDownTimer(4000,1000){
+
+                    override fun onTick(p0: Long) {
+                        itemDelete.text = String.format(context.getString(R.string.undo), (p0 / 1000))
+                    }
+
+                    override fun onFinish() {
+                        viewmodel?.deleteTask(item)
+                    }
+
+                }
+
+                //setting clicklistener for the delete button
+                var count = 0
+                itemDelete.setOnClickListener {
+                    if (count > 0){
+                        timer.cancel()
+                        itemDelete.text = context.getString(R.string.delete)
+                        count = 0
+                    } else {
+                        timer.onTick(0)
+                        timer.start()
+                        count++
+                    }
+                }
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
+
+/**
+ * Callback for calculating the diff between two non-null items in a list.
+ *
+ * Used by ListAdapter to calculate the minimum number of changes between and old list and a new
+ * list that's been passed to `submitList`.
+ */
+class TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
+}
